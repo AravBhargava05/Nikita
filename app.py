69ddf310736d5e0a26218ffe65395191cba04c48
AravBhargava05 <abhargava@college.harvard.edu>
2025-02-08 23:02:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from flask import Flask, send_from_directory
+app = Flask(__name__)
+
+CORS(app)
 
 app = Flask(__name__)
 
